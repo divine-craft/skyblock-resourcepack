06af630b13119676be407d8c307cf15eee49479d
progrm_jarvis <gh@progrm-jarvis.ru>
2020-06-17 20:35:08 +0300

    Move assets to `divinecraft` namespace

--- scripts/generate_resourcepack.sh
+++ scripts/generate_resourcepack.sh
@@ -7,13 +7,20 @@ set -ev
 mkdir -p ./target/
 find ./target/ -mindepth 1 -delete
 
+# Create directory for storing resourcepack files
 mkdir ./target/resourcepack/
 
-# Copy resourcepack files to `build` directory
+# Copy resourcepack files to maniupulated directory
 cp ./pack.mcmeta ./target/resourcepack/
 cp ./pack.png ./target/resourcepack/
 cp -r ./assets/ ./target/resourcepack/assets/
 
+# Generate multimodel files
+python ./scripts/generate_multimodels.py
+
+# Compress JSON models
+
+# Generate
 cd ./target/resourcepack/
 zip -r ../resourcepack .
 cd ../../
