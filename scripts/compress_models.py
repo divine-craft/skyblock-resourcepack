8cb30c8745350b59e276b6565d22ffedeeecd0e1
progrm_jarvis <gh@progrm-jarvis.ru>
2020-06-17 20:01:16 +0300

    Add compress_models.py

---
+++ scripts/compress_models.py
@@ -0,0 +1,51 @@
+import json
+import os
+from typing import Final
+
+# see: https://minecraft.gamepedia.com/Model
+import click
+
+ALLOWED_MODEL_ROOT_TAGS: Final = {
+    'parent', 'ambientocclusion', 'display', 'textures', 'elements', 'gui_light', 'overrides'
+}
+
+
+def compress_model_files(models_directory):
+    for parent, directories, files in os.walk(models_directory):
+        print('parent =', parent, ',', 'directories =', directories, ',', 'files =', files)
+        for file in files:
+            if not file.endswith('.json'):
+                continue
+
+            file = os.path.join(parent, file)
+            print('Handling:', file)
+            with open(file, 'r+') as model_file:
+                model = json.load(model_file)
+                model_file.seek(0)
+
+                # Patches start
+
+                print(type(model))
+                # Remove unused root rags
+                for key, _ in list(model.items()):
+                    if key not in ALLOWED_MODEL_ROOT_TAGS:
+                        del model[key]
+
+                # Patches end
+
+                json.dump(model, model_file, separators=(',', ':'))
+                model_file.truncate()
+
+
+@click.command()
+@click.option(
+    '-m', '--models', 'models_path',
+    help='Path to the folder in which all json-models should be compressed',
+    type=click.Path(file_okay=False, writable=True)
+)
+def main(models_path):
+    compress_model_files(models_path)
+
+
+if __name__ == '__main__':
+    main()
