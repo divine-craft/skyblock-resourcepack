8ac4c5b6eb6539494f0526a42ae10b3206e3c94c
progrm_jarvis <gh@progrm-jarvis.ru>
2020-06-20 15:31:11 +0300

    Make build script generate two different directories

--- scripts/generate_resourcepacks.sh
+++ scripts/generate_resourcepacks.sh
@@ -14,34 +14,34 @@ echo "Description: $DESCRIPTION"
 # Make this script fail in case of failed commands
 set -ev
 
-# Create empty directory to store files
+# Create empty directory to store build files
 mkdir -p ./target/
 find ./target/ -mindepth 1 -delete
-
-# Create directory for storing resourcepack files
-mkdir ./target/resourcepack/
-
-# Copy resourcepack files to maniupulated directory
-cp -r ./assets/ ./target/resourcepack/assets/
-cp ./pack.png ./target/resourcepack/
 
 ############################
 # Generate resourcepack V3 #
 ############################
 
-# Generate multimodel files
+# Create directory for storing resourcepack files
+mkdir ./target/resourcepack_v3/
+
+# Copy resourcepack files to manipulated directory
+cp -r ./assets/ ./target/resourcepack_v3/assets/
+cp ./pack.png ./target/resourcepack_v3/
+
+# Generate v3 multimodel files
 python3 ./scripts/generate_multimodels.py --mappings ./mappings/ \
---target ./target/resourcepack/assets/minecraft/models/item/
+--target ./target/resourcepack_v3/assets/minecraft/models/item/ --format 3
 
 # Compress JSON models
-python3 ./scripts/compress_models.py --models ./target/resourcepack/assets/divinecraft/models/
+python3 ./scripts/compress_models.py --models ./target/resourcepack_v3/assets/divinecraft/models/
 
 # Create valid pack.mcmeta
-python3 ./scripts/generate_pack_mcmeta.py --path ./target/resourcepack/ --version 3 --description "$DESCRIPTION"
+python3 ./scripts/generate_pack_mcmeta.py --path ./target/resourcepack_v3/ --version 3 --description "$DESCRIPTION"
 
 # Generate v3 ZIP-archive
 # `cd` is used not to keep full path to files
-cd ./target/resourcepack/
+cd ./target/resourcepack_v3/
 zip -r ../resourcepack_v3 .
 cd ../../
 
@@ -49,15 +49,24 @@ cd ../../
 # Generate resourcepack V4 #
 ############################
 
+# note: mkdir is *not* needed as it will change copy-semantics
+# Copy resourcepack files to manipulated directory
+cp -r ./target/resourcepack_v3/ ./target/resourcepack_v4
+
+# Generate v4 multimodel files
+rm -r ./target/resourcepack_v4/assets/minecraft/models/item/
+python3 ./scripts/generate_multimodels.py --mappings ./mappings/ \
+--target ./target/resourcepack_v4/assets/minecraft/models/item/ --format 4
+
 # Fix models
-python3 ./scripts/patch_v3_models_to_v4.py --models ./target/resourcepack/assets/divinecraft/models/
-python3 ./scripts/patch_v3_models_to_v4.py --models ./target/resourcepack/assets/minecraft/models/
+python3 ./scripts/patch_v3_models_to_v4.py --models ./target/resourcepack_v4/assets/divinecraft/models/
+python3 ./scripts/patch_v3_models_to_v4.py --models ./target/resourcepack_v4/assets/minecraft/models/
 
 # Create valid pack.mcmeta
-python3 ./scripts/generate_pack_mcmeta.py --path ./target/resourcepack/ --version 4 --description "$DESCRIPTION"
+python3 ./scripts/generate_pack_mcmeta.py --path ./target/resourcepack_v4/ --version 4 --description "$DESCRIPTION"
 
 # Generate v4 ZIP-archive
 # `cd` is used not to keep full path to files
-cd ./target/resourcepack/
+cd ./target/resourcepack_v4/
 zip -r ../resourcepack_v4 .
 cd ../../
