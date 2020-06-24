fef2df02d3b15f79e4f6f1fac6e5f7f7489c4424
FaCSM <sasa65132@gmail.com>
2020-06-24 17:06:50 +0300

    Replace python3 -> python

--- scripts/generate_resourcepacks.sh
+++ scripts/generate_resourcepacks.sh
@@ -30,14 +30,14 @@ cp -r ./assets/ ./target/resourcepack_v3/assets/
 cp ./pack.png ./target/resourcepack_v3/
 
 # Generate v3 multimodel files
-python3 ./scripts/generate_multimodels.py --mappings ./mappings/ \
+python ./scripts/generate_multimodels.py --mappings ./mappings/ \
 --target ./target/resourcepack_v3/assets/minecraft/models/item/ --format 3
 
 # Compress JSON models
-python3 ./scripts/compress_models.py --models ./target/resourcepack_v3/assets/divinecraft/models/
+python ./scripts/compress_models.py --models ./target/resourcepack_v3/assets/divinecraft/models/
 
 # Create valid pack.mcmeta
-python3 ./scripts/generate_pack_mcmeta.py --path ./target/resourcepack_v3/ --version 3 --description "$DESCRIPTION"
+python ./scripts/generate_pack_mcmeta.py --path ./target/resourcepack_v3/ --version 3 --description "$DESCRIPTION"
 
 # Generate v3 ZIP-archive
 # `cd` is used not to keep full path to files
@@ -55,15 +55,15 @@ cp -r ./target/resourcepack_v3/ ./target/resourcepack_v4
 
 # Generate v4 multimodel files
 rm -r ./target/resourcepack_v4/assets/minecraft/models/item/
-python3 ./scripts/generate_multimodels.py --mappings ./mappings/ \
+python ./scripts/generate_multimodels.py --mappings ./mappings/ \
 --target ./target/resourcepack_v4/assets/minecraft/models/item/ --format 4
 
 # Fix models
-python3 ./scripts/patch_v3_models_to_v4.py --models ./target/resourcepack_v4/assets/divinecraft/models/
-python3 ./scripts/patch_v3_models_to_v4.py --models ./target/resourcepack_v4/assets/minecraft/models/
+python ./scripts/patch_v3_models_to_v4.py --models ./target/resourcepack_v4/assets/divinecraft/models/
+python ./scripts/patch_v3_models_to_v4.py --models ./target/resourcepack_v4/assets/minecraft/models/
 
 # Create valid pack.mcmeta
-python3 ./scripts/generate_pack_mcmeta.py --path ./target/resourcepack_v4/ --version 4 --description "$DESCRIPTION"
+python ./scripts/generate_pack_mcmeta.py --path ./target/resourcepack_v4/ --version 4 --description "$DESCRIPTION"
 
 # Generate v4 ZIP-archive
 # `cd` is used not to keep full path to files
